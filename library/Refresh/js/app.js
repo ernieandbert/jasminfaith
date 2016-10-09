ea631a903ec971088e7dd48ac2619d2d3ee006b0
ernieandbert <jazz.ernest@gmail.com>
2016-10-09 21:36:50 +1100

    js

---
+++ library/Refresh/js/app.js
@@ -0,0 +1,15 @@
+$(document).foundation();
+
+$(document).ready(function(){
+	$('.mainThree').slick({
+      autoplay: true,
+      autoplaySpeed: 3000,
+      accessibility: true,
+      fade: true,
+      infinite: true,
+      swipe: true,
+      speed: 400,
+      arrows: false,
+
+    });
+});
